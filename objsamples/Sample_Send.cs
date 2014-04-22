3f50a06357d45b1c2bc156159507405be8c34499
Michael Clark <michaelallenclark@gmail.com>
2014-04-22 15:49:07 -0400

    Removed comment

--- objsamples/Sample_Send.cs
+++ objsamples/Sample_Send.cs
@@ -20,28 +20,27 @@ namespace objsamples
             s.AuthStub = myclient;
             s.SearchFilter = new SimpleFilterPart() { Property = "SendDate", SimpleOperator = SimpleOperators.greaterThan, DateValue = new DateTime[] { filterDate } };
             s.Props = new string[] { "ID", "PartnerKey", "CreatedDate", "ModifiedDate", "Client.ID", "Client.PartnerClientKey", "Email.ID", "Email.PartnerKey", "SendDate", "FromAddress", "FromName", "Duplicates", "InvalidAddresses", "ExistingUndeliverables", "ExistingUnsubscribes", "HardBounces", "SoftBounces", "OtherBounces", "ForwardedEmails", "UniqueClicks", "UniqueOpens", "NumberSent", "NumberDelivered", "NumberTargeted", "NumberErrored", "NumberExcluded", "Unsubscribes", "MissingAddresses", "Subject", "PreviewURL", "SentDate", "EmailName", "Status", "IsMultipart", "SendLimit", "SendWindowOpen", "SendWindowClose", "IsAlwaysOn", "Additional", "BCCEmail", "EmailSendDefinition.ObjectID", "EmailSendDefinition.CustomerKey" };
-            GetReturn oeGet = s.Get();
+            GetReturn sGet = s.Get();
 
-            Console.WriteLine("Get Status: " + oeGet.Status.ToString());
-            Console.WriteLine("Message: " + oeGet.Message.ToString());
-            Console.WriteLine("Code: " + oeGet.Code.ToString());
-            Console.WriteLine("Results Length: " + oeGet.Results.Length);
-            Console.WriteLine("MoreResults: " + oeGet.MoreResults.ToString());
-             //Since this could potentially return a large number of results, we do not want to print the results
-            foreach (ET_Send send in oeGet.Results)
+            Console.WriteLine("Get Status: " + sGet.Status.ToString());
+            Console.WriteLine("Message: " + sGet.Message.ToString());
+            Console.WriteLine("Code: " + sGet.Code.ToString());
+            Console.WriteLine("Results Length: " + sGet.Results.Length);
+            Console.WriteLine("MoreResults: " + sGet.MoreResults.ToString());
+            foreach (ET_Send send in sGet.Results)
             {
                 Console.WriteLine("JobID: " + send.ID + ", SendDate: " + send.SendDate );
             }
 
-            while (oeGet.MoreResults)
+            while (sGet.MoreResults)
             {
-                Console.WriteLine("Continue Retrieve Filtered SentEvents with GetMoreResults");
-                oeGet = s.GetMoreResults();
-                Console.WriteLine("Get Status: " + oeGet.Status.ToString());
-                Console.WriteLine("Message: " + oeGet.Message.ToString());
-                Console.WriteLine("Code: " + oeGet.Code.ToString());
-                Console.WriteLine("Results Length: " + oeGet.Results.Length);
-                Console.WriteLine("MoreResults: " + oeGet.MoreResults.ToString());
+                Console.WriteLine("Continue Retrieve Filtered Send with GetMoreResults");
+                sGet = s.GetMoreResults();
+                Console.WriteLine("Get Status: " + sGet.Status.ToString());
+                Console.WriteLine("Message: " + sGet.Message.ToString());
+                Console.WriteLine("Code: " + sGet.Code.ToString());
+                Console.WriteLine("Results Length: " + sGet.Results.Length);
+                Console.WriteLine("MoreResults: " + sGet.MoreResults.ToString());
             }
 
         }
